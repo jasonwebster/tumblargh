faa57f480608768e2b6a28419348de4b752f8ce7
Jason Webster <jason@metalabdesign.com>
2012-07-25 15:58:13 -0700

    Autoload the new Dialogue resource

--- lib/tumblargh/resource.rb
+++ lib/tumblargh/resource.rb
@@ -3,6 +3,7 @@ module Tumblargh
 
     autoload :Base,      'tumblargh/resource/base'
     autoload :Blog,      'tumblargh/resource/blog'
+    autoload :Dialogue,  'tumblargh/resource/dialogue'
     autoload :Note,      'tumblargh/resource/note'
     autoload :Post,      'tumblargh/resource/post'
     autoload :Tag,       'tumblargh/resource/tag'
